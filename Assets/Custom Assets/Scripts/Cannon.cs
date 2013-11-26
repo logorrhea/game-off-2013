a2b7f7424cfe478d8a5c36c67828f6483daa45db
Tyler Funk <tcfunk25@gmail.com>
2013-11-26 11:13:15 -0600

    Changed the cannons so that they face the direction you are shooting, rather than remaining still all the time. Feels much better.

--- Assets/Custom Assets/Scripts/Cannon.cs
+++ Assets/Custom Assets/Scripts/Cannon.cs
@@ -5,21 +5,27 @@ public class Cannon : MonoBehaviour {
 	
 	public GameObject bullets;
 	public float BULLET_SPEED = 60.0f;
+	private Camera mainCamera;
+	private Vector3 _lookDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		
+		mainCamera = GameObject.Find("Main Camera").camera;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		_lookDirection = mainCamera.ScreenToWorldPoint(new Vector3(
+			Input.mousePosition.x,
+			Input.mousePosition.y,
+			mainCamera.farClipPlane)).normalized;
+		transform.rotation = Quaternion.FromToRotation(Vector3.up, _lookDirection);
 	}
 	
 	public void fire(Vector3 shootDirection, Transform target) {
 		GameObject newBullet = (GameObject) Instantiate(
 			bullets,
-			transform.position + lineToTarget(target)*2,
+			transform.position + _lookDirection*2,
 			transform.rotation);
 		newBullet.rigidbody.velocity = shootDirection * BULLET_SPEED;
 	}
